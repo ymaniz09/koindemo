cb9bd64c730dde273c57edc0ee21be6ed827f2bd
Ygor <ygor.maniz@gmail.com>
2020-04-06 23:43:29 -0300

    Logging with Koin

--- app/src/main/java/com/github/ymaniz09/koindemo/KoinApplication.kt
+++ app/src/main/java/com/github/ymaniz09/koindemo/KoinApplication.kt
@@ -1,17 +1,25 @@
 package com.github.ymaniz09.koindemo
 
 import android.app.Application
+import com.github.ymaniz09.koindemo.di.KoinLogger
 import org.koin.android.ext.android.startKoin
+import timber.log.Timber
 
 class KoinApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
         startKoin(
             this, listOf(
                 applicationModule,
                 browseModule
             ),
-            loadProperties = true
+            loadProperties = true,
+            logger = KoinLogger()
         )
     }
 }
